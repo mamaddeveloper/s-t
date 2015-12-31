c293541b42d1c3b27e56a7d932dea02152ae1ec9
mamaddeveloper <mr.ashkan7786@gmail.com>
2015-12-31 19:18:00 +0330

    Update autoleave.lua

--- plugins/autoleave.lua
+++ plugins/autoleave.lua
@@ -1,5 +1,5 @@
 local function run(msg)
-	local bot_id = 142744025
+	local bot_id = 153856433
 	if msg.action and msg.action.type then
 		if msg.action.type == 'chat_add_user' and msg.action.user.id == bot_id then
 			chat_del_user('chat#id'..msg.to.id, 'user#id'..bot_id, ok_cb, true)
