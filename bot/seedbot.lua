6482a747338fec9b4861060cde0c12b13769b4fd
mamaddeveloper <mr.ashkan7786@gmail.com>
2016-01-03 17:15:08 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,7 +223,7 @@ function create_config( )
     },
     sudo_users = {94477327,144976651,173666523},--Sudo users
     disabled_channels = {},
-    realm = {92913551},--Realms Id
+    realm = {81452884},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Persia V3.0
 
