665d802602712f3e2a66f975510a7c5509d0a99e
mamaddeveloper <mr.ashkan7786@gmail.com>
2016-01-05 15:21:29 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'pershia' then -- Put everything you like :)
+  if matches[1]:lower() == 'pershian' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /telegp ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "pershia" then -- Put everything you like :)
+    if matches[2] == "pershian" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
